77136ef86ede1b31dbb15d1f27fc970b55a56227
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2022-04-13 13:53:24 +0100

    Função abrir funcia. Introdução de defesa contra palavras acentuadas

--- projnovo.c
+++ projnovo.c
@@ -4,11 +4,14 @@
 #include <time.h>
 #include <ctype.h>
 
-void ini_board (char board[15][15],int n);//função que define e imprime o tabuleiro inicial
-void print_board (char board[15][15],int n);//funçao para ser utilizada durante o programa para ver como está o tabuleiro
+void ini_board (char board[15][15],int n);
+void print_board (char board[15][15],int n);
 int modo_1 (char board[15][15],int n, int*);
 void modo_2(char board[15][15], int n);
-char **abrir(FILE* ,int);
+int point_word(char board[15][15],char palavra[50],int n,int linha,int coluna,int direcao);
+void bbc(char board[15][15], int n);
+void look_word(char board[15][15],int n,int linha,int coluna,int direcao);
+char **abrir(FILE* ,int, int*);
 
 int main(){
   char board[15][15]; //endereço do board
@@ -42,6 +45,9 @@ int main(){
     case 2:
       modo_2(board ,n);
       break;
+    case 5:
+     bbc(board,n);
+     break;
     }
   return 0;
 }
@@ -186,94 +192,179 @@ int modo_1(char board[15][15],int n, int * pontos){
 }
 
 void modo_2(char board[15][15], int n){
-  int i,j,n2,linha,coluna,centro,r=0;
-  char **word,palavra[50];
-  srand(time(0));
+  int i,j,k,linha,coluna,centro,r=0,point=0,point2=1,direcao=0,a=0,linha2=0,coluna2=0, sizeOfDic = 0;
+  char **word,palavra[50],palavra2[50],l;
   FILE *Eng;
-  FILE *Pt;
-  Eng = fopen("american-menos-mais","r");
-  Pt=fopen("pt_PT.dic","r");
-
-  word = abrir(Eng, n);
- 
-  if (word == NULL){
-    printf("Não foi possível abrir o dicionário");
-    return;
+  if((Eng = fopen("american_menos_mais","r"))==NULL){
+    printf("fds nao consegui abrir a merda do eng");
   }
 
-  do{
-    j=rand()%102401;
-    strcpy(palavra,word[j]);
-    printf("%s",palavra);
-    palavra[0]=tolower(palavra[0]);
-  }while(strlen(palavra)>n);
+  word = abrir(Eng, n, &sizeOfDic);
 
+  for(i=0;i<sizeOfDic;i++){
+       linha=0;
+       coluna=0;
+
+       strcpy(palavra,word[i]);
+       for ( j=0;j<strlen(palavra); ++j) {
+        palavra[j]=tolower( palavra[j]);
+       }
+
+       if(strlen(palavra)<=n){
+         point=point_word(board,palavra,n,linha,coluna,direcao);
+
+         if(point>point2){
+            point2=point;
+            strcpy(palavra2,palavra);
+
+             }
+         }
+  }
+  printf("%s",palavra2);
+  palavra2[0]=tolower(palavra2[0]);
+  centro=(((n/2)-(strlen(palavra2)/2))+1);
   i=0;
-  centro=n-(strlen(palavra));
-  while(palavra[i]!='\n'){
-    if(palavra[i]!='\n'){
-    board[n/2][centro]=palavra[i];
+
+  while(palavra2[i]!='\n'){
+    if(palavra2[i]!='\n'){
+    board[(n/2)][centro]=palavra2[i];
     i++;
     centro++;
     }
   }
   print_board(board,n);
-  for(linha=0;linha<n;linha++){
-      for(coluna=0;coluna<n;coluna++){
-        if((board[linha][coluna]<='z')&&(board[linha][coluna]>='a')){
-          i=0;
-          do{
-             j=rand()%102401;
-             strcpy(palavra,word[j]);
-             palavra[0]=tolower(palavra[0]);
-             printf("%s",palavra);
-             if( (palavra[0]==board[linha][coluna]) && (strlen(palavra)<=(n-linha))){
-                r++;
-             }
-             }while(r==0);
-          while(palavra[i]!='\n'){
+  printf("%s fez %d pontos",palavra2,point2);
+  direcao++;
 
-                board[linha][coluna]=palavra[i];
-                i++;
-                linha++;
+  for(i=0;i<sizeOfDic;i++){
+    free(word[i]);}
 
-            }
-        }
-      }
-  }
-  print_board(board,n);
-  for(i=0;i<=102401;i++){
-    free(word[i]);
-
-  }
   fclose(Eng);
-  fclose(Pt);
   return;
+}
+int point_word(char board[15][15],char palavra[50],int n,int linha,int coluna,int direcao){
+  int pontos_letras[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+  int a=0,points=0,k=0,f=0,j=1;
+  char i;
+  i=palavra[a];
+  while(i!='\n'&&i!='\0'){
+    if(board[linha][coluna]=='#' || ( i >  'z')||( i < 'a')){
+      return 0;
+    }else if(board[linha][coluna]=='$'){
+      f++;
+      k = (int) palavra[a] - 'a';
+      points=points+pontos_letras[k];
+
+    }else if(board[linha][coluna]=='.'){
+      k = (int) palavra[a] - 'a';
+      points=points+pontos_letras[k];
+    }else if(board[linha][coluna]=='3'){
+      k = (int) palavra[a] - 'a';
+      points=points+(pontos_letras[k]*3);
+    }else if(board[linha][coluna]=='2'){
+      k = (int) palavra[a] - 'a';
+      points=points+(pontos_letras[k]*2);
+    }else if(board[linha][coluna]>='a'&&board[linha][coluna]<='z'){
+         if(board[linha][coluna]!=palavra[a]){
+            return 0;
+         }
+    }
+    if((direcao%2)==0){
+        coluna++;
+    }else{
+        linha++;
+          }
+    a++;
+    i=palavra[a];
+    }
+    while(f>0){
+        points=points*2;
+        f--;
+    }
+
+    return points;
+}
+void bbc(char board[15][15], int n){
+    int point=0,linha=n/2,coluna=4,direcao=0;
+    char palavra[50];
+    printf("diga as coordenadas estrubilho\n");
+    scanf("%d",&linha);
+    scanf("%d",&coluna);
+    scanf("%d",&direcao);
+
+    look_word( board, n, linha, coluna, direcao);
+
+    return;
+}
+void look_word(char board[15][15],int n,int linha,int coluna,int direcao){
+  char **word,palavra[50],palavra2[50];
+  int length,i,r,point=0,point2=1,point3=2, sizeOfDic = 0;
+  FILE *Eng;
+  if((Eng = fopen("american_menos_mais","r"))==NULL){
+    printf("fds nao consegui abrir a merda do eng");
+  }
+
+  word = abrir(Eng, n, &sizeOfDic);
+  for(i=0;i<sizeOfDic;i++){
+    strcpy(palavra,word[i]);
+    for ( int j=0;j<strlen(palavra); ++j) {
+        palavra[j]=tolower( palavra[j]);
+       }
+    length=strlen(palavra);
+    if(length>=(n/2)){
+        r=n-length;
+        if((direcao%2)==0){
+             coluna=0;
+        }else{linha=0;}
+        while(r>=0){
+             if((direcao%2)==0){
+                point=point_word(board,palavra,n,linha,coluna,direcao);
+                coluna++;
+             }else{
+                 point=point_word(board,palavra,n,linha,coluna,direcao);
+                 linha++;
+             }
+             r--;
+             if(point2<point){
+                point2=point;
+             }
+        }
+     if(point3<point2){
+        point3=point2;
+        strcpy(palavra2,palavra);
+     }
+    }
+
+}
+printf("%s com %d",palavra2,point3);
+return;
 }
 /*Função de abertura de dicionáro modificado do lab 5
   Toma como parâmetro o apontador para o ficheiro a abrir e o tamanho do tabuleiro*/
-char** abrir(FILE *run, int n){
+char** abrir(FILE *run, int n, int *sizeOfFile){
   char buffer[100], **linhas;
   int size = 0, i = 0;
+  *sizeOfFile = 0;
 
   //alocação dinâmica do número de palavras (só pomos em memória as palavras com o tamanho do tauleiro)
-  while(!(feof(run) || size > n)){
+  while(!(feof(run) || size > n + 1)){
     size = 0;
     fgets(buffer, 100, run);
     size = strlen(buffer);
-    i++;
+    *sizeOfFile = *sizeOfFile + 1;
   }
-
-  linhas = (char**) calloc(i, sizeof(char*));
-  i = 0;
-  if ( linhas[i] == NULL){
+  linhas = (char**) calloc(*sizeOfFile, sizeof(char*));
+  if ( linhas == NULL){
     printf("Erro de memória");
-    free(linhas[i]);
+    free(linhas);
     return NULL;
   }
 
+  //colocar stream no início do ficheiro
+  fseek(run, 0, SEEK_SET);
+
   //alocação dinâmica do tamanho das palavras e armazenamento das mesmas em memória
-  while( !(feof(run) || size > n +1)){
+  do{
     size = 0;
     fgets(buffer, 100, run);
     size = strlen(buffer) + 1;
@@ -287,6 +378,7 @@ char** abrir(FILE *run, int n){
       strcpy(linhas[i], buffer);
     }
     i++;
-  }
+  }while( !(feof(run) || size > n + 2));
+
   return linhas;
 }
